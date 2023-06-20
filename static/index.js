adaa6bf94cfd8e97406efc7a8ce5f5e9f0dca13d
grite123454321 <133250983+grite123454321@users.noreply.github.com>
2023-06-20 20:20:52 +0000

    apps and games

--- static/index.js
+++ static/index.js
@@ -1,16 +1,48 @@
+function $(id){
+    return document.getElementById(id);
+}
 //menu
-function openmenu() {
-    document.getElementById("menu").style.left = "0";
+function openMenu(id) {
+    $(id).style.left = "0";
 }
 
-function closemenu() {
-    document.getElementById("menu").style.left = "100%";
+function closeMenu(id) {
+    $(id).style.left = "100%";
 }
-//settings
-function openSettings() {
-    document.getElementById("settingsmenu").style.width = "100%";
+
+//games and apps
+var searchag;
+function agSwap(ag){
+    apps = $('apps');
+    games = $('games');
+    if(ag == "g"){
+        games.style.cssText = "display: initial;";
+        apps.style.cssText = "display: none;";
+    }
+    else if(ag == "a"){
+        apps.style.cssText = "display: initial;";
+        games.style.cssText = "display: none;";
+    }
 }
-  
-function closeSettings() {
-    document.getElementById("settingsmenu").style.width = "0%";
+
+function filter(){
+    if($('apps').style.cssText == 'display: initial;'){
+        container = document.getElementById('apps');
+    }
+    if($('games').style.cssText == 'display: initial;'){
+        container = document.getElementById('games');
+    }
+    var input, filter, container, button, p, i, txtValue;
+    input = document.getElementById("agsearch");
+    filter = input.value.toUpperCase();
+    button = container.getElementsByTagName("button");
+    for (i = 0; i < button.length; i++) {
+        p = button[i].getElementsByTagName("p")[0];
+        txtValue = p.textContent || p.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            button[i].style.display = "";
+        } else {
+            button[i].style.display = "none";
+        }
+    }
 }
