b7e71a7889f7a8333d32136c878dd3e491d015c3
grite123454321 <133250983+grite123454321@users.noreply.github.com>
2023-07-19 19:10:20 +0000

    settings update

--- static/assets/files/js/index.js
+++ static/assets/files/js/index.js
@@ -46,3 +46,29 @@ function filter(){
         }
     }
 }
+
+//settings
+
+function save(){
+    name = $("tabtext").value;
+    icon = $("favicon").value;
+    window.localStorage.setItem('name', name);
+    window.localStorage.setItem('icon', favicon);
+    reset();
+}
+
+function setDefault(){
+    window.localStorage.setItem('name', "My Drive - Google Drive");
+    window.localStorage.setItem('icon', "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png");
+    window.location.href = window.location;
+    reset();
+}
+function reset(){
+    var currentUrl = window.location.href;
+    var mainDomainUrl = currentUrl.replace(/\/[^\/]*$/, '');
+    if (window != top) {
+        top.location.href = mainDomainUrl;
+    } else {
+        window.location.href = mainDomainUrl;
+    }   
+}
