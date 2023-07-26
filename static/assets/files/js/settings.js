022cc13e71407430ea6e7360cb411887ef90a106
grite123454321 <133250983+grite123454321@users.noreply.github.com>
2023-07-26 21:53:18 +0000

    settings fix

--- static/assets/files/js/settings.js
+++ static/assets/files/js/settings.js
@@ -1,20 +1,37 @@
 //settings
 
-function save(){
-    name = $("tabtext").value;
-    icon = $("favicon").value;
-    window.localStorage.setItem('name', name);
+window.onload = (event) => {
+    alert($('fullscreenswitch').innerHTML);
+    $("tabtext").value = window.localStorage.getItem('name');
+    $("favicon").value = window.localStorage.getItem('icon');
+
+    if(window.localStorage.getItem('fs?').includes('checked') == true){
+        $('fullscreenswitch').innerHtml = "<input type='checkbox' checked><span class='slider round'></span>";
+    }
+    else{
+        $('fullscreenswitch').innerHtml = "<input type='checkbox'><span class='slider round'></span>";
+    }
+};
+
+function saveSettings(){
+    tabtext = $("tabtext").value;
+    favicon = $("favicon").value;
+    fs = $('fullscreenswitch').innerHtml;
+    window.localStorage.setItem('name', tabtext);
     window.localStorage.setItem('icon', favicon);
-    reset();
+    window.localStorage.setItem('fs?', fs);
+    resetPage();
 }
 
 function setDefault(){
     window.localStorage.setItem('name', "My Drive - Google Drive");
     window.localStorage.setItem('icon', "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png");
+    window.localStorage.setItem('fs?', "<input type='checkbox'><span class='slider round'></span>")
     window.location.href = window.location;
-    reset();
+    resetPage();
 }
-function reset(){
+
+function resetPage(){
     var currentUrl = window.location.href;
     var mainDomainUrl = currentUrl.replace(/\/[^\/]*$/, '');
     if (window != top) {
