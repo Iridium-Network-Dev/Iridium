a2ec85a3525f7b7bd9adb0ce2690bb1fea0039eb
grite123454321 <133250983+grite123454321@users.noreply.github.com>
2023-07-27 19:11:42 +0000

    yay

--- static/assets/files/js/settings.js
+++ static/assets/files/js/settings.js
@@ -3,31 +3,23 @@
 window.onload = () => {
     $("tabtext").value = window.localStorage.getItem('name');
     $("favicon").value = window.localStorage.getItem('icon');
-
-    if(window.localStorage.getItem('fs?').includes('checked')){
-        $('fullscreenswitch').innerHtml = "<input type='checkbox' checked><span class='slider round'></span>";
-    }
-    else{
-        $('fullscreenswitch').innerHtml = "<input type='checkbox'><span class='slider round'></span>";
-    }
+    $('fullscreenswitch').checked = window.localStorage.getItem('fs?');
 };
 
-function settingSwitch(id){
-    var switch1 = $(id).innerHTML;
-    if(switch1.includes("checked")){
-        alert();
-        switch1 = "<input type='checkbox'><span class='slider round'></span>";
+$("fullscreenswitch").addEventListener( "change", () => {
+    var theSwitch = $("fullscreenswitch");
+    if(theSwitch.checked){
+        theSwitch.checked = false;
     }
     else{
-        alert();
-        switch1 = "<input type='checkbox' checked><span class='slider round'></span>";
+        theSwitch.checked = true;
     }
-}
+ });
 
 function saveSettings(){
     tabtext = $("tabtext").value;
     favicon = $("favicon").value;
-    fs = $('fullscreenswitch').innerHtml;
+    fs = $('fullscreenswitch').checked;
     window.localStorage.setItem('name', tabtext);
     window.localStorage.setItem('icon', favicon);
     window.localStorage.setItem('fs?', fs);
@@ -37,8 +29,7 @@ function saveSettings(){
 function setDefault(){
     window.localStorage.setItem('name', "My Drive - Google Drive");
     window.localStorage.setItem('icon', "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png");
-    window.localStorage.setItem('fs?', "<input type='checkbox'><span class='slider round'></span>")
-    window.location.href = window.location;
+    window.localStorage.setItem('fs?', false);
     resetPage();
 }
 
