dc377453473591ddeb8c5154d9bf39d41361aefc
grite123454321 <133250983+grite123454321@users.noreply.github.com>
2023-12-13 00:41:42 +0000

    message script tweak

--- static/assets/files/js/advertise.js
+++ static/assets/files/js/advertise.js
@@ -1,6 +1,8 @@
 function ad(){
-    if(window.localStorage.getItem("test") == null){
+    if(window.localStorage.getItem("q") == null){
         alert('If you want to display a message whenever this link loads for once, please go to link+"/ad.html" for more info.');
-        window.localStorage.setItem("test", "yes")
+        if(Math.floor(Math.random() * 8) == 0){
+            window.localStorage.setItem("q", "yes")
+        }
     }
 }
