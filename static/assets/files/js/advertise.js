7e3cfb5052303f68774065d29816fa0a5102a1c4
grite123454321 <133250983+grite123454321@users.noreply.github.com>
2023-12-13 13:23:18 +0000

    ad.js start

--- static/assets/files/js/advertise.js
+++ static/assets/files/js/advertise.js
@@ -1,8 +1,9 @@
 function ad(){
-    if(window.localStorage.getItem("q") == null){
+    var num = Math.floor(Math.random() * 8);
+    if(window.localStorage.getItem("ad?") == null){
         alert('If you want to display a message whenever this link loads for once, please go to link+"/ad.html" for more info.');
-        if(Math.floor(Math.random() * 8) == 0){
-            window.localStorage.setItem("q", "yes")
+        if(num == 0){
+            window.localStorage.setItem("ad?", "yes");
         }
     }
 }
