17237a2acacdd5338ce2ae9a22b03b7b6ef874a8
grite123454321 <133250983+grite123454321@users.noreply.github.com>
2024-01-05 02:27:08 +0000

    new ad

--- static/assets/files/js/advertise.js
+++ static/assets/files/js/advertise.js
@@ -1,10 +1,10 @@
 function ad(){
-    //var num = Math.floor(Math.random() * 5);
-    if(1==1){
-        //alert('If you want to display a message whenever this link loads for once, please go to link+"/ad.html" for more info.');
-        alert('Happy Winter Break!')
-       /* if(num == 0){
-            window.localStorage.setItem("ad?", "yes");
-        }*/
+    var num = Math.floor(Math.random() * 5);
+    if(window.localStorage.getItem("ad") != 1){
+        alert('If you want to display a message whenever this link loads for once, please go to link+"/ad.html" for more info.');
+        alert('Fuck school. - Anonymous');
+        if(num == 0){
+            window.localStorage.setItem("ad", "1");
+        }
     }
 }
