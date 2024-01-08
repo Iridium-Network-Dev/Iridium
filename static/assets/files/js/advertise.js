fa70dc825a57ea59a101e3bad23602d34a46f400
grite123454321 <133250983+grite123454321@users.noreply.github.com>
2024-01-08 13:09:59 +0000

    aa

--- static/assets/files/js/advertise.js
+++ static/assets/files/js/advertise.js
@@ -1,10 +1,9 @@
 function ad(){
     var num = Math.floor(Math.random() * 5);
-    if(window.localStorage.getItem("ad") != 1){
+    if(window.localStorage.getItem("aa") != 1){
         alert('If you want to display a message whenever this link loads for once, please go to link+"/ad.html" for more info.');
-        alert('Fuck school. - Anonymous');
         if(num == 0){
-            window.localStorage.setItem("ad", "1");
+            window.localStorage.setItem("aa", "1");
         }
     }
 }
